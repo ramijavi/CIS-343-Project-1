679b7ffe9883febce69c97d58f71093d0e6a4942
Javier Ramirez Moyano <ramijavi@eos21.cis.gvsu.edu>
2018-02-01 18:21:08 -0500

    Initial commit. We created a header file and a main method, along with some notes.

---
+++ file_utilities.h
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#ifndef file_utilities.h
+#define file_utilities.h
+
+
+// This function will read data from a chosen file
+int read_file (char*, char**);
+
+// This function will save data onto a file
+int write_file(char*, char*, int);
+
+
+#endif
