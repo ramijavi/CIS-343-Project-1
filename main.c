c8a4f5d9a83b2dcd7376fdd9ae0f8ac5cf09feef
Javier Ramirez Moyano <ramijavi@eos19.cis.gvsu.edu>
2018-02-02 15:03:21 -0500

    Changed main to try to print cmd lines

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "file_utilities.h"
+
+void initilize();
 
 int main(int argc, char** argv[]){
 	
-	printf("You are playing The Game of Life");
+	printf("Welcome player. You are playing the game of life\n");
+	printf("You have chosen to play with a board of size : %s x %s", agrv[1], agrv[2]);
+}
+
+void initialize_board(){
+
 	
 }
