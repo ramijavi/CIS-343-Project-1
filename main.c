b5c41e6b88b6ba81de86e99c9796cf6319359484
Javier Ramirez Moyano <ramijavi@eos12.cis.gvsu.edu>
2018-02-06 15:55:18 -0500

    included a printBoard function and changed the memory allocation function

--- main.c
+++ main.c
@@ -11,59 +11,78 @@
  * In this assignment we will focus our attention mostly on memory allocation & memory
  * management in general. 
  */
+
 #include "file_utilities.h"
 
-void initilize();
 
-int allocateBoard(char h, char w){
+// Global variables declaration
+// A double pointer holds a pointer to the board
+// Two integrers hold the height and width
+int **board;
+int height, width;
 
-	int r = h, c = w, i, j, count;
+// This function allocates memory to hold the 2D array
+// that holds the board. It uses the function malloc
+void allocateBoard(){
+
+	int r = height, c = width, i, j;
  
-   	int **arr = (int **)malloc(r * sizeof(int *));
+   	board = (int **)malloc(r * sizeof(int *));
 	for (i=0; i<r; i++){
-        	arr[i] = (int *)malloc(c * sizeof(int));
+        	board[i] = (int *)malloc(c * sizeof(int));
  	}
-  	
-	count = 0;
+  
         for (i = 0; i <  r; i++){
         	for (j = 0; j < c; j++){
-                	arr[i][j] = ++count;  // OR *(*(arr+i)+j) = ++count
+                	board[i][j] = 1;  // OR *(*(arr+i)+j) = ++count
                 }
-	}
-        
-	for (i = 0; i <  r; i++){
-        	for (j = 0; j < c; j++){
-                	printf("%d ", arr[i][j]);
-                }                          
-        }                             
-        
-	// return 0;
+	}                             
 }
+
+// This function prints the board out
+void printBoard(){
+
+	int i, j;
+
+	printf("\n");
+
+	for (i = 0; i <  height; i++){
+		
+		printf("| ");
+                
+		for (j = 0; j < width; j++){
+                        printf("%d", board[i][j]);
+			printf(" | ");
+        	}
+
+
+		printf("\n");
+        }
+}
+
 
 int main(int argc, char* argv[]){
 
-	int *boardLocation;
-
 	// Greet the player when the game is initialized
-	printf("Welcome player. You are playing the game of life\n");
+	printf("\nWelcome player. You are playing the game of life\n");
 	
 	// Check if the player entered correct input
 	// Correct input should be two numbers 
 	if(argc < 3 || argc > 3){
 		// If input was not correct, the player is notified 
 		// and the game is exited
-		printf("User did not specify height and width of game. Exiting...");
+		printf("\nUser did not specify height and width of game. Exiting...");
 		return 0;
 	}
 	// If input is correct, then it's shown to the player
-	printf("You have chosen to play with a board of size : %s x %s \n", argv[1], argv[2]);
+	printf("\nYou have chosen to play with a board of size : %s x %s \n", argv[1], argv[2]);
 	
-	int h = atoi(argv[1]);
-	int w = atoi(argv[2]);
+	height = atoi(argv[1]);
+	width = atoi(argv[2]);
 	
-	allocateBoard(h, w);
-
-	
+	allocateBoard();
+ 	
+	printBoard();
 /*
 	char* height;
 	height = malloc(strlen(argv[1]));
@@ -74,7 +93,7 @@ int main(int argc, char* argv[]){
 	width = malloc(strlen(argv[2]));
 	width = strcat(width, argv[2]);
 	printf("width is: %s \n", width);
-*/	
+	
 
 	printf("Would you like to open an existing saved generation?\n");
 	char openSave;
@@ -93,7 +112,7 @@ int main(int argc, char* argv[]){
 	printf("openSave: %c \n",openSave);
 	free(height);
 	free(width);
-
+*/
 	return 0;
 }
 
