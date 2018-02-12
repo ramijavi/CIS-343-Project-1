7b41031c59ed472ef75a78305630107af515c185
Javier Ramirez Moyano <ramijavi@eos12.cis.gvsu.edu>
2018-02-12 00:47:09 -0500

    Made some comments on the main, more to come

--- main.c
+++ main.c
@@ -2,18 +2,17 @@
  * CIS 343 - 02 Structures of Programming Languages
  * Project 1 - The Game of Life
  * 
- * @authors - Andy hing Hung & Javier Ramirez
- * 
  * The Game of Life was developed by John Conway (a British mathematician) in the 1970s. 
  * It is an example of a cellular automaton, a data structure that seems to evolve iteratively.
  * In this simulation we will use a set of rules to determine the survival or death of the cells.
  *
  * In this assignment we will focus our attention mostly on memory allocation & memory
  * management in general. 
+ *
+ * @authors - Andy Hing Hung & Javier Ramirez
  */
 
 #include "file_utilities.h"
-
 
 int main(int argc, char* argv[]){
 
@@ -24,49 +23,56 @@ int main(int argc, char* argv[]){
 	int keepPlaying = 1;	
 	char openSave;
         char fileName[20];
+	char fileSave[20];
 	char doNext;
 	int runGen;	
 
 	/* Greet the player when the game is initialized */
 	printf("\nWelcome player. You are playing the game of life\n");
 	
-	/* Ask user if they would like to open a file */
+	/* Ask user if they want to start playing the game */
 	printf("To start the game, type 'y'. Or to close, type a random letter.\n");
 	scanf("%c", &openSave);
 	printf("\n");
+
 	/* If user wants to open a file */
 	if(openSave == 'y'){
 		/* Ask the player for the file name */
 		printf("Enter the file name you wish to open. 10x10.txt or 20x20.txt \n");
 		scanf("%s", fileName);
-		/* Display file name*/
+
+		/* Display the file name*/
 		printf("The fileName is: %s \n", fileName);
+
 		/* Call a function to read the file. We pass the file name */
 		/* and the address of the board variable */
 		read_file(fileName, &board);	
-		/* Get the dimensions and generation of the new board */
+
+		/* Get the dimensions, generation and size of the new board */
 		height = getHeight(fileName);
 		width = getWidth(fileName);
 		generation = getGen(fileName);
-		/* Allocating memory for the temp board */
+		size = getSize(fileName);		
+
+		/* Allocating memory for the temporary board */
 		allocateBoard(&tempBoard,height,width);
+
 		/* Display the board dimensions and generation */
 		printf("The file was succesfully loaded\n");
 		printf("The height is %d, width is, %d and gen is %d \n", height, width, generation);
-		size = getSize(fileName);
-		printf("size is: %d",size);
-		/* Display board */
-		printBoard(board,height,width);
-		//printBoard(tempBoard,height,width);
+		printf("The file size is: %d",size);
 		
-
+		/* Display board */
+		printBoard(board,height,width);		
 	}
-	/* If user do not want to open a file */
+	/* If user does not want to open a file */
 	else{
+		/* Print a closing message and exit application */
 		printf("You didn't type 'y'. Closing..\n");
 		exit(0);
 	}
 	
+	/* This loop will execute until the player wants to exit the game */
 	while (keepPlaying == 1){
 		/* Ask the user what they want to do next */
 		printf("What would you like to do next?\nEnter 's' to save file, 'l' to load a file, ");
@@ -75,16 +81,24 @@ int main(int argc, char* argv[]){
 		scanf("  %c", &doNext);
 		
 		if(doNext == 's'){
+		/* If user enters 's', then the game will be written onto a file */
+			
+			/* Ask user for file name */
+			printf("What name do you want to save your game under?\n");
+			scanf(" %s", fileSave);
 
-
+			/* Call save function */
+			
 		} else if(doNext == 'l'){
-		/* If user neters 'l', a saved generation will be loaded */
+		/* If user enters 'l', a saved generation will be loaded */
 			
 			/* Ask the player for the file name */
 	                printf("Enter the file name you wish to open. \n");
         	        scanf("%s", fileName);
+
                		/* Display file name*/
          	       	printf("The fileName is: %s \n", fileName);
+
  	        	/* Call a function to free both boards */
         	        freeBoard(&board, height);
         	        freeBoard(&tempBoard, height);
